5011355a902da165b311eee76fc800b95da17158
unknown <rajatis1999@gmail.com>
2020-07-10 18:47:14 +0530

    coursera scrap resolved

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-const express = require('express');
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let rp = require("request-promise");
+let $ = require("cheerio");
+
 const app = express();
-const bodyParser = require('body-parser');
+const youtubeSearchRoutes = require('./api/routes/youtube_search');
 
-
-const youtubeSearchRoutes = require('./routes/youtube_search');
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type,X-Request-With");
+    if(req.method==="OPTION"){
+        res.sendStatus(200);
+    }
+    next();
+});
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use((req, res, next) =>{
-	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","*");  //'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	if(req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-	next();
+//Coursera Part
+rp('https://www.coursera.org/search?query=web%20development&skipBrowseRedirect=true')
+.then((html) => {
+    let name = $('.ais-InfiniteHits-item',html);
+    for(let i=0;i<name.length;i++){
+    console.log("Name : ",name[i].children[0].children[0].children[0].children[0].children[0].children[0].children[0].attribs.alt);
+    console.log("ImgLink : ",name[i].children[0].children[0].children[0].children[0].children[0].children[0].children[0].attribs.src);
+    console.log("Partner Name : ",name[i].children[0].children[0].children[0].children[0].children[0].children[0].next.children[0].next.children[0].children[0].data);
+    console.log("Course Type : ",name[i].children[0].children[0].children[0].children[0].children[0].children[0].next.children[0].next.next.children[0].children[0].data,"\n");
+    };
+
+
+
 });
 
+//Youtube Route
 app.use('/youtubeSearch', youtubeSearchRoutes);
+
+app.get(express.static(path.join(__dirname,'frontend/build')));
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
 
 
 app.use((req, res, next) =>{
@@ -36,5 +59,6 @@ app.use((error, req, res, next) => {
 	});
 })
 
-
-module.exports = app;
+app.listen(process.env.PORT || 8000,() => {
+    console.log("Server is running");
+}); 
