645566d99e83a6ed64075f12d494df40774a18f2
Shikhar Gupta <46660993+StrangerZiokan@users.noreply.github.com>
2020-07-30 23:08:15 +0530

    Add files via upload

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+
+
+module.exports = (req, res, next) => {
+	//const decoded = jwt.decode();
+	try{
+		const token = req.headers.authorization.split(" ")[1];
+		//console.log(token);
+		const decoded = jwt.verify(token, process.env.JWT_KEY);	
+		req.userData = decoded;
+		next();
+	}
+	catch(error){
+		return res.status(401).json({
+			message: 'Authorizationss Failed'
+		});
+	}
+	
+	
+};
