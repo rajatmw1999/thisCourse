59c74c407853b77d2a7acfe77a5d346cfa177f83
aradhna7 <aradhna.7sharma@gmail.com>
2020-08-23 01:38:30 +0530

    footer and outermost category opt

--- convertedReact/src/components/outermost-category/index.js
+++ convertedReact/src/components/outermost-category/index.js
@@ -3,43 +3,110 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class OutermostCategory extends React.Component {
-    
-    
-    render() {
-      return <div className="outer-div-outermost">
-      <div class="container">
+  render() {
+    return (
+      <div className='container  outer-div-outermost'>
+        <div className='row form-group grp1'>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/book-reading.png'
+            />
+            <p className='para col-8'>Computer science</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/stationery.png'
+            />
+            <p className='para col-8'>Art and Design</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/handshake.png'
+            />
+            <p className='para col-8'>Business</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg biggerheading col-4'
+              src='https://img.icons8.com/color/48/000000/combo-chart.png'
+              // style={{ marginBottom: '23px' }}
+            />
+            <p className='para col-8'>Personal Development</p>
+          </div>
+        </div>
 
-      <div class="row form-group grp1" >
-         <div class="col-md-3 all">	<img src="https://img.icons8.com/color/48/000000/book-reading.png" class="cs"/><p class="para_1">Computer science</p>
-		</div>
-        <div class="col-md-3 all" ><img src="https://img.icons8.com/color/48/000000/stationery.png" class="AD"/><p class="para_2">Art and Design</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/handshake.png"class="BSNS"/><p class="para_3">Business</p></div>
-         <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/combo-chart.png" class="PD"/><p class="para_4">Personal Development</p></div>
+        <div className='row form-group grp2'>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/satellites.png'
+              
+            />
+            <p className='para col-8'>Data science</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4 biggerheading'
+              // style={{ marginBottom: '23px' }}
+              src='https://img.icons8.com/color/48/000000/network.png'
+              
+            />
+            <p className='col-8 para'>Physical science and engineering</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/omnichannel.png'
+              
+            />
+            <p className='para col-8'>Marketing</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4 biggerheading'
+              src='https://img.icons8.com/color/48/000000/communication.png '
+              // style={{ marginBottom: '23px' }}
+            />
+            <p className=' col-8 para'>Language and communication</p>
+          </div>
+        </div>
+
+        <div className='row form-group'>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/orchid.png'
+            />
+            <p className='para col-8'>Life sciences</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/deviation.png'
+            />
+            <p className='para col-8'>Math and Logic</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/conference-call.png'
+            />
+            <p className='para col-8'>Social sciences</p>
+          </div>
+          <div className='col-12 col-lg-3 all'>
+            <img
+              className='aradhna_outimg col-4'
+              src='https://img.icons8.com/color/48/000000/heart-health.png'
+            />
+            <p className='para col-8'>Health and fitness</p>
+          </div>
+        </div>
       </div>
-
-
-	 <div class="row form-group grp2" >
-         <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/satellites.png" class="DS"/><p class="para_5">Data science</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/network.png" class="PSC"/><p class="para_6">Physical science
-        <br/> and engineering</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/omnichannel.png"class="Maktng "/><p class="para_7">Marketing</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/communication.png"class="Landc"/><p class="para_8">Language
-        <br/>and communication</p></div>
-
-      </div>
-
-      <div class="row form-group">
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/orchid.png"/><p class="para_9">Life sciences</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/deviation.png"/><p class="para_10">Math and Logic</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/conference-call.png"/><p class="para_11">Social sciences</p></div>
-        <div class="col-md-3 all"><img src="https://img.icons8.com/color/48/000000/heart-health.png"/><p class="para_12">Health and fitness</p></div>
-
-      </div>
-</div>
-</div>
-;
-     
+    );
   }
-  }
-  
-  export default OutermostCategory;
+}
+
+export default OutermostCategory;
