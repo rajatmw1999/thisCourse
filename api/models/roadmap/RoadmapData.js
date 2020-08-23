9d5bb1bda5a5882534cfd86f16fd5d225de3e787
shivam7374 <shivamsingla491@gmail.com>
2020-08-23 21:00:58 +0530

    Models and Routes

---
+++ api/models/roadmap/RoadmapData.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Roadmap_Data = new mongoose.Schema({
+  NameOfRoadmap: {
+    type: String,
+  },
+  NameofAuthor: {
+    type: String,
+  },
+  DatePublished: {
+    type: Date,
+  },
+  Likes: {
+    type: Number,
+  },
+  ImageLink: {
+    type: URL,
+  },
+  Category: {
+    type: String,
+  },
+  Tags: [
+    {
+      type: String,
+    },
+  ],
+  Text: {
+    type: String,
+  },
+  Timeline: [
+    {
+      name: {
+        type: String,
+      },
+      desc: {
+        type: String,
+      },
+      time: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+module.exports = RoadmapData = mongoose.model("RoadmapData", Roadmap_Data);
