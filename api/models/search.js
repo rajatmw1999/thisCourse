974d2691df31be27f7f80a2661730d3429f84650
Shikhar Gupta <46660993+StrangerZiokan@users.noreply.github.com>
2020-08-30 20:47:57 +0530

    Update search.js

--- api/models/search.js
+++ api/models/search.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
+const tagSchema = mongoose.Schema({
+	tagName: {type: String}
+});
+
 const searchSchema = mongoose.Schema({
-	q: {type:String, required: true}
-	
+	q: [{type:String}],
+	nameofSkill: {type:String},
+	googleApiName: {type:String},
+	tags: [tagSchema]
 });
 
 module.exports = mongoose.model ('Search',searchSchema);
