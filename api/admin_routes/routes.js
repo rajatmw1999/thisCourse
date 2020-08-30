112ca2f10eec97a8b2daebd3e15b36441b7a1599
shivam7374 <shivamsingla491@gmail.com>
2020-08-30 13:59:34 +0530

    Route Completed

--- api/admin_routes/routes.js
+++ api/admin_routes/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const FeaturedCourses = require("../models/roadmap/FeaturedCourses");
 
+//Route 1
 router.post("/addfeaturedcourses", (req, res, next) => {
   const featuredcourse = new FeaturedCourses({
     _id: new mongoose.Types.ObjectId(),
@@ -26,20 +27,19 @@ router.post("/addfeaturedcourses", (req, res, next) => {
     });
 });
 
+//Route 2
 router.post("/featuredCoursesById", (req, res) => {
   const id = req.body.id;
-  RoadmapData.find({ _id: id })
+  FeaturedCourses.find({ _id: id })
     .exec()
     .then((doc) => {
       console.log("From database", doc);
       if (doc) {
         res.status(200).json(doc);
       } else {
-        res
-          .status(404)
-          .json({
-            message: "No valid entry found for provided FeaturedCourses",
-          });
+        res.status(404).json({
+          message: "No valid entry found for provided FeaturedCourses",
+        });
       }
     })
     .catch((err) => {
@@ -47,3 +47,43 @@ router.post("/featuredCoursesById", (req, res) => {
       res.status(500).json({ error: err });
     });
 });
+
+//Route 3
+router.post("/featuredCoursesByCategorySkill", (req, res) => {
+  const category = req.body.category;
+  const skillname = req.body.skillName;
+  FeaturedCourses.find({ NameOfSkill: skillname, Category: category })
+    .exec()
+    .then((doc) => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: "No valid entry found for provided FeaturedCourses",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+//Route 4
+router.delete("/deleteFeaturedCoursesById", (req, res, next) => {
+  const id = req.body.productId;
+  FeaturedCourses.remove({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
