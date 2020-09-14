0c431757543d968c8e114ef1cf1859ecb2445b1d
shivam7374 <shivamsingla491@gmail.com>
2020-09-14 21:12:54 +0530

    Add

--- api/routes/categories/academics.js
+++ api/routes/categories/academics.js
@@ -1,34 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const apnacourse = require('../../middleware/apnacourse');
-const arcademics = require('../../middleware/arcademics');
-const bitdegree = require('../../middleware/bitdegree');
-const codecademy = require('../../middleware/codeacademy');
-const coursera = require('../../middleware/coursera');
-const digigrad = require('../../middleware/digigrad');
-const edx = require('../../middleware/edx');
-const harvard = require('../../middleware/harvard');
-const khanacademy = require('../../middleware/khanAcademy');
-const openyale = require('../../middleware/openYale');
-const skillshare = require('../../middleware/skillshare');
-const udacity = require('../../middleware/udacity');
-const udemy = require('../../middleware/udemy');
-const verbling = require('../../middleware/verbling');
+const apnacourse = require("../../middleware/apnacourse");
+const arcademics = require("../../middleware/arcademics");
+const bitdegree = require("../../middleware/bitdegree");
+const codecademy = require("../../middleware/codeacademy");
+const coursera = require("../../middleware/coursera");
+const digigrad = require("../../middleware/digigrad");
+const edx = require("../../middleware/edx");
+const harvard = require("../../middleware/harvard");
+const khanacademy = require("../../middleware/khanAcademy");
+const openyale = require("../../middleware/openYale");
+const skillshare = require("../../middleware/skillshare");
+const udacity = require("../../middleware/udacity");
+const udemy = require("../../middleware/udemy");
+const verbling = require("../../middleware/verbling");
 
+router.get("/", (req, res, next) => {
+  // apnacourse(req,res,next,'data structure','academics');
+  // arcademics(req,res,next,'data structure','academics');
+  // bitdegree(req,res,next,'data structure','academics');
+  // codecademy(req,res,next,'data structure','academics');
+  // digigrad(req,res,next,'data structure','academics');
+  // edx(req,res,next,'data structure','academics');
+  // harvard(req,res,next,'data structure','academics');
+  // khanacademy(req,res,next,'data structure','academics');
+  // openyale(req,res,next,'data structure','academics');
+  // skillshare(req,res,next,'data structure','academics');
+  // udacity(req,res,next,'data structure','academics');
+  // verbling(req,res,next,'data structure','academics');
 
+  // coursera(req,res,next,'data structure','academics');
+  // udemy(req, res, next, "data structure", "academics");
 
-
-router.get("/",(req,res,next) => {
-
-    // coursera(req,res,next,'data structure','academics');
-    // udemy(req,res,next,'data structure','academics');
-    
-    console.log('Inside Academics');
-	res.status(200).json({
-		message:'Search Results for academics !'
-		// query: query.q,
-	});
+  console.log("Inside Academics");
+  res.status(200).json({
+    message: "Search Results for academics !",
+    // query: query.q,
+  });
 });
 
 module.exports = router;
