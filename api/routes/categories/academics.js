859b78455bbac6578893361aad0df194933e4251
shivam7374 <shivamsingla491@gmail.com>
2020-09-14 21:49:45 +0530

    Add

--- api/routes/categories/academics.js
+++ api/routes/categories/academics.js
@@ -52,7 +52,7 @@ router.get("/", (req, res, next) => {
   //   udemy(req, res, next, "algorithms", "academics");
   //   verbling(req, res, next, "algorithms", "academics");
 
-  //   apnacourse(req, res, next, "electronics", "academics");
+  apnacourse(req, res, next, "electronics", "academics");
   //   arcademics(req, res, next, "electronics", "academics");
   //   bitdegree(req, res, next, "electronics", "academics");
   //   codecademy(req, res, next, "electronics", "academics");
