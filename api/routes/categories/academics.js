a40f5bf34d0dabe5b3ac4c4c33b06da801010719
shivam7374 <shivamsingla491@gmail.com>
2020-09-14 23:48:45 +0530

    Add

--- api/routes/categories/academics.js
+++ api/routes/categories/academics.js
@@ -126,8 +126,8 @@ router.get("/", (req, res, next) => {
   // bitdegree(req, res, next, "calculus", "academics");
   // codecademy(req, res, next, "calculus", "academics");
   // coursera(req, res, next, "calculus", "academics");
-  digigrad(req, res, next, "calculus", "academics");
-  //   edx(req, res, next, "calculus", "academics");
+  // digigrad(req, res, next, "calculus", "academics");
+  edx(req, res, next, "calculus", "academics");
   //   harvard(req, res, next, "calculus", "academics");
   //   khanacademy(req, res, next, "calculus", "academics");
   //   openyale(req, res, next, "calculus", "academics");
