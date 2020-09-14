733e578498380d825edb622a7bd599c88444e804
shivam7374 <shivamsingla491@gmail.com>
2020-09-14 21:23:24 +0530

    Add

--- api/routes/categories/academics.js
+++ api/routes/categories/academics.js
@@ -27,8 +27,8 @@ router.get("/", (req, res, next) => {
   //   digigrad(req, res, next, "data structure", "academics");
   //   edx(req, res, next, "data structure", "academics");
   //   harvard(req, res, next, "data structure", "academics");
-  khanacademy(req, res, next, "data structure", "academics");
-  // openyale(req,res,next,'data structure','academics');
+  //   khanacademy(req, res, next, "data structure", "academics");
+  openyale(req, res, next, "data structure", "academics");
   // skillshare(req,res,next,'data structure','academics');
   // udacity(req,res,next,'data structure','academics');
   // verbling(req,res,next,'data structure','academics');
